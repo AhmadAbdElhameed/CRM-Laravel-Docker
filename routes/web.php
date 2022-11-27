a8710bed45a28fa31e9de3dc706d1a2205f7e34b
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2022-11-27 02:06:44 +0200

    test routing , migartion and controller are success

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index',[App\Http\Controllers\UsersController::class,'index']);
+
 Route::get('/hello', function () {
     return "Hello Page";
 });
